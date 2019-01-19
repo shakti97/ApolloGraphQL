9de1edc59288962209379965b220c8785895deaf
shakti97 <shaktisingh230797@gmail.com>
2019-01-19 23:29:08 +0530

    Refactor the Apollo Server with the resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,7 +1,21 @@
 const {ApolloServer} = require('apollo-server');
+const { createStore } = require('./utils');
 const typeDefs=require('./schema');
+const resolvers = require('./resolvers');
 
-const server=new ApolloServer({typeDefs});
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server=new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources : ()=>({
+        launchAPI : new LaunchAPI(),
+        UserAPI : new UserAPI({store}) 
+    })
+});
 
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
