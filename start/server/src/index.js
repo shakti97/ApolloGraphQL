0bccff5a99eccd01ac67e5a4bff951eeb23d5162
shakti97 <shaktisingh230797@gmail.com>
2019-02-07 16:05:51 +0530

    added Authentication

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,18 +2,29 @@ const {ApolloServer} = require('apollo-server');
 const { createStore } = require('./utils');
 const typeDefs=require('./schema');
 const resolvers = require('./resolvers');
-
+const isEmail=require('isemail');
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
 
 const store = createStore();
 
 const server=new ApolloServer({
+    context : async ({req})=>{
+        const auth = (req.headers && req.headers.authorization)|| '';
+        const email = Buffer.from(auth,'base64').toString('ascii');
+        //if the email is not validated return the user as null
+        if(!isEmail.validate(email)) return {user : null};
+
+        //adter veriication find the uer by it email
+        const users=await store.users.findOrCreate({where : {email}});
+        const user = users && users[0] ? users[0] : null;
+        return {user : {...user.dataValues} };
+    },
     typeDefs,
     resolvers,
     dataSources : ()=>({
         launchAPI : new LaunchAPI(),
-        UserAPI : new UserAPI({store}) 
+        userAPI : new UserAPI({store}) 
     })
 });
 
