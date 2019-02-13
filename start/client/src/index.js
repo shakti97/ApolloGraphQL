ca78e06c2c462539c5b4230c36796b1b33d8158f
shakti97 <shaktisingh230797@gmail.com>
2019-02-13 23:08:42 +0530

    Issue Fixed

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,25 +1,6 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import gql from "graphql-tag";
+import { ApolloProvider } from 'react-apollo';
 
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'http://localhost:4000/'
-})
-const client = new ApolloClient({
-  cache,
-  link
-})
-client.query({
-        query : gql`
-            query GetLaunch{
-                launch(id : 56){
-                    id
-                    mission {
-                        name
-                        }  
-                    }
-                }
-            `
-    }).then(result=>console.log(result));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>, document.getElementById('root'));
