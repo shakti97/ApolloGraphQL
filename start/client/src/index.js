1bd185480e53d960c29d1f927d61d31c5b15bd05
shakti97 <shaktisingh230797@gmail.com>
2019-02-20 17:31:13 +0530

    Login Functionality

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,5 +1,24 @@
 import { ApolloProvider } from 'react-apollo';
 
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      authorization: localStorage.getItem('token'),
+    },
+  }),
+  resolvers,
+  typeDefs,
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Pages />
